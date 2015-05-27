6f61f51fc9013155450b6b24756b2c3973153f52
SiccarPoint <dan.hobley@gmail.com>
2015-05-27 15:16:56 -0600

    Modernizing scarp diffusion tutorial
    scarp_diffusion_with_component.py is now 1. consistent with current
    best practice, 2. consistent w the webpage description of it, and most
    importantly, 3. functional.

--- scripts/diffusion/scarp_diffusion_with_component.py
+++ scripts/diffusion/scarp_diffusion_with_component.py
@@ -1,7 +1,7 @@
 #Import statements so that you will have access to the necessary functions
 import numpy
 from landlab import RasterModelGrid
-from landlab.components.diffusion.diffusion import DiffusionComponent
+from landlab.components.diffusion.diffusion import LinearDiffuser
 from landlab.plot.imshow import imshow_node_grid
 from pylab import show, figure
 
@@ -11,7 +11,8 @@ mg = RasterModelGrid(25, 40, 10.0)
 
 #Create a field of node data (an array) on the grid called elevation.  
 #Initially populate this array with zero values.
-z = mg.add_zeros('node', 'elevation')
+#The component wants the elevation field to be called 'topographic__elevation'
+z = mg.add_zeros('node', 'topographic__elevation')
 
 #Check the size of the array
 len(z)
@@ -22,20 +23,24 @@ upthrown_nodes = numpy.where(mg.node_y>fault_y)
 z[upthrown_nodes] += 10.0 + 0.01*mg.node_x[upthrown_nodes]
 
 #Illustrate the grid
-imshow_node_grid(mg, z, cmap='jet', grid_units=['m','m'])
+imshow_node_grid(mg, 'topographic__elevation', cmap='jet', grid_units=['m','m'])
 show()
 
 #Instantiate the diffusion component:
-linear_diffuse = DiffusionComponent(grid=mg, input_stream='./diffusion_input_file.txt')
+linear_diffuse = LinearDiffuser(grid=mg, input_stream='./diffusion_input_file.txt')
 
 #Set boundary conditions
 mg.set_closed_boundaries_at_grid_edges(False, True, False, True)
 
+#set a model timestep
+#(the component will subdivide this as needed to keep things stable)
+dt = 2000.
+
 #Evolve landscape
 for i in range(25):
-    mg = linear_diffuse.diffuse(mg)
+    linear_diffuse.diffuse(dt)
 
 #Plot new landscape
 figure()
-imshow_node_grid(mg, z, cmap='jet', grid_units=['m','m'])
+imshow_node_grid(mg, 'topographic__elevation', cmap='jet', grid_units=['m','m'])
 show()
